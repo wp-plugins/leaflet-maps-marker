d14d2d9da2aed80e749303cf90e4ffa6ea3287dc
harmr <harmr@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-10-01 15:56:05 +0000

    hotfix for 3.5beta1 compatibility
    git-svn-id: https://plugins.svn.wordpress.org/leaflet-maps-marker/trunk@606593 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- leaflet-maps-marker.php
+++ leaflet-maps-marker.php
@@ -9,7 +9,7 @@ Author: Robert Harm
 Author URI: http://www.harm.co.at
 Donate link: http://www.mapsmarker.com/donations
 Requires at least: 3.0
-Tested up to: 3.4.2
+Tested up to: 3.5beta1
 Requires at least PHP 5.2
 Copyright 2011-2012 - @RobertHarm - All rights reserved
 MapsMarker &reg; - registration pending
