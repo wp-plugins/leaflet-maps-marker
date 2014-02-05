665ca1700506203c483e625ffeb55ed99975b4f0
harmr <harmr@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-02-05 18:16:01 +0000

    hotfix - conditional SSL loading for Google JS (for better performance for users in China)
    git-svn-id: https://plugins.svn.wordpress.org/leaflet-maps-marker@852120 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- tags/3.8.4/leaflet-maps-marker.php
+++ tags/3.8.4/leaflet-maps-marker.php
@@ -45,17 +45,13 @@ if ( is_admin() ) {
 }
 
 //info: define necessary paths and urls
-if ( ! defined( 'LEAFLET_WP_ADMIN_URL' ) )
-	define( 'LEAFLET_WP_ADMIN_URL', get_admin_url() );
-if ( ! defined( 'LEAFLET_PLUGIN_URL' ) )
-	define ("LEAFLET_PLUGIN_URL", plugin_dir_url(__FILE__));
-if ( ! defined( 'LEAFLET_PLUGIN_DIR' ) )
-	define ("LEAFLET_PLUGIN_DIR", plugin_dir_path(__FILE__));
+define( 'LEAFLET_WP_ADMIN_URL', get_admin_url() );
+define ("LEAFLET_PLUGIN_URL", plugin_dir_url(__FILE__));
+define ("LEAFLET_PLUGIN_DIR", plugin_dir_path(__FILE__));
 $lmm_upload_dir = wp_upload_dir();
-if ( ! defined( 'LEAFLET_PLUGIN_ICONS_URL' ) )
-	define ("LEAFLET_PLUGIN_ICONS_URL", $lmm_upload_dir['baseurl'] . "/leaflet-maps-marker-icons");
-if ( ! defined( 'LEAFLET_PLUGIN_ICONS_DIR' ) )
-	define ("LEAFLET_PLUGIN_ICONS_DIR", $lmm_upload_dir['basedir'] . DIRECTORY_SEPARATOR . "leaflet-maps-marker-icons");
+define ("LEAFLET_PLUGIN_ICONS_URL", $lmm_upload_dir['baseurl'] . "/leaflet-maps-marker-icons");
+define ("LEAFLET_PLUGIN_ICONS_DIR", $lmm_upload_dir['basedir'] . DIRECTORY_SEPARATOR . "leaflet-maps-marker-icons");
+
 class Leafletmapsmarker
 {
 	function __construct() {
@@ -603,12 +599,13 @@ class Leafletmapsmarker
 		}
 		//info: Google API key
 		if ( isset($lmm_options['google_maps_api_key']) && ($lmm_options['google_maps_api_key'] != NULL) ) { $google_maps_api_key = $lmm_options['google_maps_api_key']; } else { $google_maps_api_key = ''; }
+		$protocol_handler = (is_ssl() == TRUE) ? 'https' : 'http'; //info: conditional ssl loading for Google js (performance issues in China)
 		//info: fallback for adding js to footer 1
 		if ( (version_compare( $wp_version, '3.3', '>=' )) && ($lmm_options['misc_javascript_header_footer'] == 'footer') ) {
-			wp_register_script( 'leafletmapsmarker-googlemaps-loader', 'https://www.google.com/jsapi?key='.$google_maps_api_key, array(), 3.7, true);
+			wp_register_script( 'leafletmapsmarker-googlemaps-loader', $protocol_handler . '://www.google.com/jsapi?key='.$google_maps_api_key, array(), 3.7, true);
 		} else if ( (version_compare( $wp_version, '3.3', '<' )) || ((version_compare( $wp_version, '3.3', '>=' )) && ($lmm_options['misc_javascript_header_footer'] == 'header')) ) {
 			wp_enqueue_script( array ( 'jquery' ) );
-			wp_enqueue_script( 'leafletmapsmarker-googlemaps-loader', 'https://www.google.com/jsapi?key='.$google_maps_api_key, array(), NULL);
+			wp_enqueue_script( 'leafletmapsmarker-googlemaps-loader', $protocol_handler . '://www.google.com/jsapi?key='.$google_maps_api_key, array(), NULL);
 		}
 		//info: Bing culture code
 		if ($lmm_options['bingmaps_culture'] == 'automatic') {
@@ -654,7 +651,8 @@ class Leafletmapsmarker
 		wp_enqueue_script( array ( 'jquery' ) );
 		//info: Google API key
 		if ( isset($lmm_options['google_maps_api_key']) && ($lmm_options['google_maps_api_key'] != NULL) ) { $google_maps_api_key = $lmm_options['google_maps_api_key']; } else { $google_maps_api_key = ''; }
-		wp_enqueue_script( 'leafletmapsmarker-googlemaps-loader', 'https://www.google.com/jsapi?key='.$google_maps_api_key, array(), NULL);
+		$protocol_handler = (is_ssl() == TRUE) ? 'https' : 'http'; //info: conditional ssl loading for Google js (performance issues in China)
+		wp_enqueue_script( 'leafletmapsmarker-googlemaps-loader', $protocol_handler . '://www.google.com/jsapi?key='.$google_maps_api_key, array(), 3.7, true);
 		//info: Bing culture code
 		if ($lmm_options['bingmaps_culture'] == 'automatic') {
 			if ( defined('WPLANG') ) { $bing_culture = WPLANG; } else { $bing_culture =  'en_us'; }
