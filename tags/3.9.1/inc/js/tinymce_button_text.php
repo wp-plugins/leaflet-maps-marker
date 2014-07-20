8b477694ba7e790f06b1e04d81bd2114b3c4790d
harmr <harmr@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-07-20 08:50:48 +0000

    prepare tag for 3.9.1
    git-svn-id: https://plugins.svn.wordpress.org/leaflet-maps-marker@951764 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ tags/3.9.1/inc/js/tinymce_button_text.php
@@ -0,0 +1,17 @@
+<?php
+header('Content-Type: text/javascript; charset=UTF-8'); //info: to prevent console warning on chrome
+$leaflet_plugin_url = isset($_GET['leafletpluginurl']) ? htmlspecialchars(base64_decode($_GET['leafletpluginurl'])) : '';
+$admin_url = isset($_GET['adminurl']) ? htmlspecialchars(base64_decode($_GET['adminurl'])) : '';
+$text_add = isset($_GET['textadd']) ? htmlspecialchars(base64_decode($_GET['textadd'])) : '';
+
+echo "jQuery(function($) {
+	$(document).ready(function(){
+		$('#map_button_text').remove();
+		$(document).on('click', '#map_button_visual', function(){
+			tb_show('" . $text_add . "', '" . $admin_url . "admin-ajax.php?action=get_mm_list&mode=html&TB_iframe');
+				return false;
+		});
+		$('#wp-content-media-buttons').append('<a style=\'margin-left:5px;\' class=\'button\' title=\'" . $text_add . "\' id=\'map_button_visual\' href=\'#\'><div style=\'float:left;\'><img src=\'" . $leaflet_plugin_url . "inc/img/icon-tinymce.png#\' style=\'padding:0 5px 3px 0;\'></div><div style=\'float:right;padding-top:0px;\'>" . $text_add . "</div></a>');
+	});
+});";
+?>
